f46414130be2c6a7f25525e3acd58b03930888d8
tonyonlian <540340102@qq.com>
2017-07-01 12:08:59 +0800

    edit main

--- hello.js
+++ hello.js
@@ -1,10 +1,11 @@
 function main(req,res,moudle){
 	var id = req.params.id
-	return 0;
-	console.log('helloWorld');
 	var a =1;
 	var b =2;
 	var c =a+b;
 	console.log(c);
+	console.log('this is change');
+	console.log('helloWorld');
+	return 0;
 
 }
